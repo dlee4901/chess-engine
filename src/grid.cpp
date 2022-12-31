62262e76798e917b55994ee3ea04d630d3c6ff68
dlee4901 <daniel.lee4901@gmail.com>
2022-12-31 17:02:37 -0600

    added more movement options

--- src/grid.cpp
+++ src/grid.cpp
@@ -37,36 +37,218 @@ std::vector< std::pair<int, int> > Grid::transform(std::vector< std::pair<int, i
     return squares;
 }
 
+std::vector< std::pair<int, int> > Grid::project(std::vector< std::pair<int, int> > &squares, std::string type, int distance)
+{
+    for (std::pair<int, int> square : squares)
+    {
+        int x = square.first;
+        int y = square.second;
+        int dist = 0;
+        if (type == "step")
+        {
+
+        }
+        else if (type == "orthogonal")
+        {
+            dist = 0;
+            for (int i = x+1; i < max_x; i++)
+            {
+                if (dist > distance || get_entity(i, y).get_type() != "null") break;
+                squares.push_back(std::pair(i, y));
+                dist += 1;
+            }
+            
+            dist = 0;
+            for (int i = y+1; i < max_y; i++)
+            {
+                if (dist > distance || get_entity(x, i).get_type() != "null") break;
+                squares.push_back(std::pair(x, i));
+            }
+            
+            dist = 0;
+            for (int i = x-1; i >= 0; i--)
+            {
+                if (dist > distance || get_entity(i, y).get_type() != "null") break;
+                squares.push_back(std::pair(i, y));
+            }
+            
+            dist = 0;
+            for (int i = y-1; i >= 0; i--)
+            {
+                if (dist > distance || get_entity(x, i).get_type() != "null") break;
+                squares.push_back(std::pair(x, i));
+            }
+        }
+        else if (type == "diagonal")
+        {
+            dist = 0;
+            x = square.first + 1;
+            y = square.second + 1;
+            while (x < max_x && y < max_y)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x += 1;
+                y += 1;
+            }
+            
+            dist = 0;
+            x = square.first + 1;
+            y = square.second - 1;
+            while (x < max_x && y >= 0)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x += 1;
+                y -= 1;
+            }
+            
+            dist = 0;
+            x = square.first - 1;
+            y = square.second - 1;
+            while (x >= 0 && y >= 0)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x -= 1;
+                y -= 1;
+            }
+            
+            dist = 0;
+            x = square.first - 1;
+            y = square.second + 1;
+            while (x >= 0 && y < max_y)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x -= 1;
+                y += 1;
+            }
+        }
+        else if (type == "line")
+        {
+            dist = 0;
+            for (int i = x+1; i < max_x; i++)
+            {
+                if (dist > distance || get_entity(i, y).get_type() != "null") break;
+                squares.push_back(std::pair(i, y));
+                dist += 1;
+            }
+            
+            dist = 0;
+            for (int i = y+1; i < max_y; i++)
+            {
+                if (dist > distance || get_entity(x, i).get_type() != "null") break;
+                squares.push_back(std::pair(x, i));
+            }
+            
+            dist = 0;
+            for (int i = x-1; i >= 0; i--)
+            {
+                if (dist > distance || get_entity(i, y).get_type() != "null") break;
+                squares.push_back(std::pair(i, y));
+            }
+            
+            dist = 0;
+            for (int i = y-1; i >= 0; i--)
+            {
+                if (dist > distance || get_entity(x, i).get_type() != "null") break;
+                squares.push_back(std::pair(x, i));
+            }
+            
+            dist = 0;
+            x = square.first + 1;
+            y = square.second + 1;
+            while (x < max_x && y < max_y)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x += 1;
+                y += 1;
+            }
+            
+            dist = 0;
+            x = square.first + 1;
+            y = square.second - 1;
+            while (x < max_x && y >= 0)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x += 1;
+                y -= 1;
+            }
+            
+            dist = 0;
+            x = square.first - 1;
+            y = square.second - 1;
+            while (x >= 0 && y >= 0)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x -= 1;
+                y -= 1;
+            }
+            
+            dist = 0;
+            x = square.first - 1;
+            y = square.second + 1;
+            while (x >= 0 && y < max_y)
+            {
+                if (dist > distance || get_entity(x, y).get_type() != "null") break;
+                squares.push_back(std::pair(x, y));
+                dist += 1;
+                x -= 1;
+                y += 1;
+            }
+        }
+    }
+    return squares;
+}
+
 std::vector< std::pair<int, int> > Grid::get_movement_options(int x, int y)
 {
     std::vector< std::pair<int, int> > movement_options;
-    movement_options.push_back(std::pair(x, y));
     Entity piece = grid[x][y]; 
-    std::string name = piece.get_name();
+    if (piece.get_type() == "null")
+    {
+        std::cout << "NULL piece" << std::endl;
+        return movement_options;
+    }
+    movement_options.push_back(std::pair(x, y));
     std::vector<Movement> movements = piece.get_movements();
     for (Movement movement : movements)
     {
+        std::string condition = movement.get_condition();
         std::string action = movement.get_action();
+        std::string parameters = movement.get_parameters();
+
+        std::unordered_map<std::string, bool> valid_directions = movement.get_valid_directions();
         std::vector<std::string> moves = split(action, " ");
         for (std::string move : moves)
         {
-            int loc_x = x;
-            int loc_y = y;
             int distance = 0;
             std::string direction = "";
-            for (int i = 0; i < move.length(); i++)
+            for (unsigned int i = 0; i < move.length(); i++)
             {
                 if (distance == 0 && isalpha(move[i]))
                 {
                     std::string dist = move.substr(0, i);
                     if (dist == "*") { distance = -1; }
-                    else             { distance = stoi(dist); }
+                    else             { distance = std::stoi(dist); }
                     direction = move.substr(i);
                     break;
                 }
             }
-
-            if (direction == "N")       { transform(movement_options, 0, distance); }
+            if (distance < 0) distance = std::max(max_x, max_y);
+            if (!valid_directions[direction]) { std::cout << "INVALID DIRECTION: " << direction << std::endl; }
+            else if (direction == "N")  { transform(movement_options, 0, distance); }
             else if (direction == "E")  { transform(movement_options, distance, 0); }
             else if (direction == "S")  { transform(movement_options, 0, -distance); }
             else if (direction == "W")  { transform(movement_options, -distance, 0); }
@@ -74,6 +256,7 @@ std::vector< std::pair<int, int> > Grid::get_movement_options(int x, int y)
             else if (direction == "SE") { transform(movement_options, distance, -distance); }
             else if (direction == "SW") { transform(movement_options, -distance, -distance); }
             else if (direction == "NW") { transform(movement_options, -distance, distance); }
+            else                        { project(movement_options, direction, distance); }
         }
     }
     return movement_options;
@@ -86,9 +269,9 @@ Entity Grid::get_entity(int x, int y)
 
 void Grid::print_grid()
 {
-    for (int i = max_x-1; i > -1; i--)
+    for (int j = max_y-1; j > -1; j--)
     {
-        for (int j = max_y-1; j > -1; j--)
+        for (int i = 0; i < max_x; i++)
         {
             Entity entity = get_entity(i, j);
             if (entity.get_type() == "null")
